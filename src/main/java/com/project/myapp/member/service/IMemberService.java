0e1bbae49d7b243e7c374f1ecb2096b02d7578db
hannahjjjj <wh467925@gmail.com>
2023-07-18 18:08:18 +0900

    이메일인증

--- src/main/java/com/project/myapp/member/service/IMemberService.java
+++ src/main/java/com/project/myapp/member/service/IMemberService.java
@@ -2,6 +2,8 @@ package com.project.myapp.member.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import com.project.myapp.member.model.Companion;
 import com.project.myapp.member.model.Member;
 
@@ -14,11 +16,14 @@ public interface IMemberService {
 	void updateMember(Member member); //회원정보 수정
 	void deleteMember(Member member); //회원탈퇴
 	
-	void findPassword(String memberId); //비밀번호 찾기
-	void updatePassword(String memberId, String password); //비밀번호 수정
+
+	public void sendEmail(Member member, String mail); //임시비밀번호 이메일발송
+	void findPassword(HttpServletResponse response, Member member); //비밀번호 찾기
+	void updatePassword(Member member); //임시비밀번호받으면 임시비밀번호로 업데이트
+	void updatePasswordByMember(Member member); //비밀번호 수정
 	
 	void insertPassenger(Companion companion); //동승자 정보 추가
 	
-	void viewReservation(String memberId); //예약 조회
+	Member viewReservation(String memberId); //예약 조회
 	
 }
--- src/main/webapp/WEB-INF/views/member/login.jsp
+++ src/main/webapp/WEB-INF/views/member/login.jsp
@@ -12,6 +12,7 @@
 	<input type="text" name="memberId">
 	<input type="password" name="password">
 	<input type="submit" value="로그인">
+	<input type="button" onclick="location.href='/myapp/member/findpwd'" class="btn btn-info" value="비밀번호 찾기">
 </form>
 </body>
 </html>
